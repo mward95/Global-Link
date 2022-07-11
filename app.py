d5b87b06d780cbc9a5f10de46fe2ce2eba896066
Matthew Ward <wardmatt95@gmail.com>
2022-07-10 21:24:38 -0400

    linked continents to the home page and started to add graphs

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 #################################################
 app.config["MONGO_URI"] = "mongodb://localhost:27017/phonebrand"
 mongo = PyMongo(app)
-# Route to renter index.html template using data from Mongo
+# Route to renter projectindex2.html template using data from Mongo
 # note to ***self*** the goal is to take each route and pass the backend data to the front end where the user selects the continent they want to explore
 # home route
 @app.route("/")
@@ -28,11 +28,11 @@ def index():
     # ww_data = mongo.db.worldwide.find()
     # print(ww_data)
 
-    # return render_template("index.html")
+    # return render_template("projectindex2.html")
     default_data = mongo.db.phone.find()
     print(default_data)
 
-    return render_template("index.html", articles=default_data)
+    return render_template("projectindex2.html", articles=default_data)
 # worlwide
 #################################################
 @app.route("/worldwide")
@@ -40,7 +40,7 @@ def world():
     ww_data = mongo.db.worldwide.find()
     print(ww_data)
 # ***the new html page that we make for the continent***
-    return render_template("index.html", mission_to_mars=ww_data)
+    return render_template("worldwide.html", phone_link=ww_data)
 # africa
 #################################################
 @app.route("/africa")
@@ -49,7 +49,7 @@ def africa():
     africa_data = mongo.db.africa.find()
     print(africa_data)
 
-    return render_template("index.html", mission_to_mars=africa_data)
+    return render_template("africa.html", phone_link=africa_data)
 # asia
 #################################################
 @app.route("/asia")
@@ -57,7 +57,7 @@ def asia():
     asia_data = mongo.db.asia.find()
     print(asia_data)
 
-    return render_template("index.html", mission_to_mars=asia_data)
+    return render_template("asia.html", phone_link=asia_data)
 # oceania
 #################################################
 @app.route("/oceania")
@@ -65,7 +65,7 @@ def oceania():
     oceania_data = mongo.db.oceania.find()
     print(oceania_data)
 
-    return render_template("index.html", mission_to_mars=oceania_data)
+    return render_template("oceania.html", phone_link=oceania_data)
 # southamerica
 #################################################
 @app.route("/southamerica")
@@ -73,7 +73,7 @@ def southamerica():
     southamerica_data = mongo.db.southamerica.find()
     print(southamerica_data)
 
-    return render_template("index.html", mission_to_mars=southamerica_data)
+    return render_template("southamerica.html", phone_link=southamerica_data)
 # europe
 #################################################
 @app.route("/europe")
@@ -81,7 +81,7 @@ def europe():
     europe_data = mongo.db.europe.find()
     print(europe_data)
 
-    return render_template("index.html", mission_to_mars=europe_data)
+    return render_template("europe.html", phone_link=europe_data)
 # northamerica
 #################################################
 @app.route("/northamerica")
@@ -89,15 +89,8 @@ def northamerica():
     northamerica_data = mongo.db.northamerica.find()
     print(northamerica_data)
 
-    return render_template("index.html", mission_to_mars=northamerica_data)
+    return render_template("northamerica.html", phone_link=northamerica_data)
   
-# @app.route("/scrape")
-# def scrape():
-#     scraped_data = mongo.db.scraped_data
-#     mars_data = scrape_mars.scrape()
-#     scraped_data.update({}, mars_data, upsert=True)
-#     return redirect("/", code=302)
-
 if __name__ == "__main__":
     app.run(debug=True)
 
